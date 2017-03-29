73a8e10a125cb69f160e1b596ab4ab0a2d74fac1
Savvy_Through <savithruml@users.noreply.github.com>
2017-03-29 15:06:44 -0700

    Update webcam.py

--- source/webcam.py
+++ source/webcam.py
@@ -3,7 +3,7 @@ import cv2
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():
     rval, frame = vc.read()
 else:
     rval = False
@@ -12,6 +12,6 @@ while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27:
         break
 cv2.destroyWindow("preview")
