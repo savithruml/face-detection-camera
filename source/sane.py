14f1a969f7accd6797cf0a7d8b9f78ed60c9a0f0
Savvy_Through <savithruml@users.noreply.github.com>
2017-03-29 15:06:11 -0700

    Update sane.py

--- source/sane.py
+++ source/sane.py
@@ -4,5 +4,5 @@ from matplotlib import pyplot as plt
  
 img = cv2.imread('rhosp.jpg', 0)
 plt.imshow(img, cmap='gray', interpolation='bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.xticks([]), plt.yticks([])
 plt.show()
